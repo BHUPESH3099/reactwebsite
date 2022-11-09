5eb0a11649b89177cd08bb67712bae86d2aa7ba2
techgig <66059140+BHUPESH3099@users.noreply.github.com>
2022-11-10 01:20:06 +0530

    Update Navbar.jsx

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -7,7 +7,7 @@ const navbar = () => {
     <div className="n-wrapper" id="Navbar">
       {/* left */}
       <div className="n-left">
-        <div className="n-name">Bhupesh</div>
+        <div className="n-name">Home Tutor</div>
         <Toggle />
       </div>
       {/* right */}
